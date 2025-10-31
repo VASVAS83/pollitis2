c5ccb1066ee2f6c2dcc1d8b766a33c800368eb14
VASVAS83 <vasmaker@gmail.com>
2025-10-31 17:11:12 +0200

    Refactor URL handling in GET request

--- app/callback/route.ts
+++ app/callback/route.ts
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export const GET = async (request: Request) => {
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get('code');
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
