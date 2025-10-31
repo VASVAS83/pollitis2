e97cd0f266b28371420e415479835d84c661a830
VASVAS83 <vasmaker@gmail.com>
2025-10-31 16:14:01 +0200

    Implement Google OAuth sign-in route

---
+++ app/auth/google/route.ts
@@ -0,0 +1,12 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export const POST = async () => {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { data } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: { redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}` }
+  });
+  return NextResponse.redirect(data.url);
+};
