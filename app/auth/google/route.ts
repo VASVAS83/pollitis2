cb9e43d7f0635be3e2fd17fc8d5bbdf9318c2161
VASVAS83 <vasmaker@gmail.com>
2025-10-31 17:07:45 +0200

    Refactor authentication handler for Google sign-in

--- app/auth/google/route.ts
+++ app/auth/google/route.ts
@@ -3,7 +3,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-const handleAuth = async () => {
+const handler = async () => {
   const supabase = createRouteHandlerClient({ cookies });
   const { data } = await supabase.auth.signInWithOAuth({
     provider: 'google',
@@ -17,5 +17,4 @@ const handleAuth = async () => {
     : NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_SITE_URL!));
 };
 
-export const GET = handleAuth;
-export const POST = handleAuth;
+export { handler as GET, handler as POST };
